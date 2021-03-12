1a79cee790212416f1398d0e696e47a377db4212
Nikita <nikkarchinskiy@gmail.com>
2021-03-12 14:20:30 +0200

    Task1-2

---
+++ Hw/4/task_2.js
@@ -0,0 +1,7 @@
+const total = 100;
+const order = 20;
+if (total => order) {
+    console.log ("Заказ оформлен, с вами свяжется менеджер")
+   }   else {
+        console.log ("На складе недостаточно твоаров");
+    };
