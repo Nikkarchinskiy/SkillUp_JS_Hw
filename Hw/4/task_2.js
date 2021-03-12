915145f49785d7167cb2944b331ebb6f43ed5f5c
Nikita <nikkarchinskiy@gmail.com>
2021-03-12 15:03:01 +0200

    fix bags

--- Hw/4/task_2.js
+++ Hw/4/task_2.js
@@ -1,6 +1,6 @@
 const total = 100;
 const order = 20;
-if (total => order) {
+if (total >= order) {
     console.log ("Заказ оформлен, с вами свяжется менеджер")
    }   else {
         console.log ("На складе недостаточно твоаров");
